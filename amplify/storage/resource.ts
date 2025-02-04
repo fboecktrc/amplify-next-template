ef95005bbc0ee5e5ad3f61116bea4025447e0b8b
Florian B <fboeck@tecracer.de>
2025-02-04 22:55:24 +0100

    add: amplify storage

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,12 @@
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+    name: "web-vite-app",
+    access: (allow: any) => ({
+        'public/*': [
+            allow.guest.to(['get', 'list']),
+            allow.authenticated.to(['read', 'write', 'delete']),
+            allow.entity('admin').to(['read', 'write', 'delete'])
+        ]
+    })
+})
